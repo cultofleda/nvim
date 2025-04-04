06fa29d43f1771a3d82c6c9ac05990eec317c3df
cultofleda <info@cultofleda.studio>
2025-04-04 13:34:33 +0200

    style: add linebreaks for better readability

--- lua/keymaps/main.lua
+++ lua/keymaps/main.lua
@@ -6,11 +6,13 @@ vim.keymap.set("n", "<C-h>", "<CMD>wincmd h<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<CMD>wincmd j<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<CMD>wincmd k<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<CMD>wincmd l<CR>", { silent = true })
+
 -- Resize splits
 vim.keymap.set("n", "<C-A-h>", "<CMD>vertical resize +5<CR>", { silent = true })
 vim.keymap.set("n", "<C-A-j>", "<CMD>horizontal resize -5<CR>", { silent = true })
 vim.keymap.set("n", "<C-A-k>", "<CMD>horizontal resize +5<CR>", { silent = true })
 vim.keymap.set("n", "<C-A-l>", "<CMD>vertical resize -5<CR>", { silent = true })
+
 -- Rebind macro recording
 vim.keymap.set("n", "q", "<nop>", { noremap = true })
 vim.keymap.set("n", "Q", "q", { noremap = true, desc = "Record macro" })
