87511fec9497f4d0758a64ba84fb8822c83ee9f7
cultofleda <info@cultofleda.studio>
2025-04-04 10:54:15 +0200

    enable hybrid line numbers (relative + absolute on current line)

--- lua/opts/main.lua
+++ lua/opts/main.lua
@@ -12,6 +12,7 @@ vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.mouse = "a"
 vim.opt.relativenumber = true
+vim.opt.number = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
