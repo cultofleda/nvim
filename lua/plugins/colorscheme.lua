a654c105eadf0f033d194b007d5fae3e3637133d
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2025-07-07 09:17:37 +0200

    chore(colorscheme): use `poimandres.nvim`

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
-    "rockyzhang24/arctic.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    name = "arctic",
-    branch = "main",
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme arctic")
-    end,
+    {
+        "olivercederborg/poimandres.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("poimandres").setup({})
+        end,
+        init = function()
+            vim.cmd("colorscheme poimandres")
+        end,
+    },
 }
