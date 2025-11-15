7cd14279d4d0c4c5d36afeaf9c211a1f5c9f0c83
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2025-11-15 13:06:48 +0100

    refactor(colorscheme): debloat colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,10 @@
 return {
     {
-        "Vallen217/eidolon.nvim",
-        lazy = false,
-        priority = 1000,
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
         config = function()
-            vim.cmd("colorscheme eidolon-twilight")
-        end,
-    },
-    {
-        "olivercederborg/poimandres.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("poimandres").setup({})
-        end,
-        init = function()
-            -- vim.cmd("colorscheme poimandres")
-        end,
-    },
-    {
-        "marko-cerovac/material.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("material").setup({})
-        end,
-        init = function()
-            -- vim.g.material_style = "deep ocean"
-            -- vim.cmd("colorscheme material")
+            require("onedarkpro").setup({})
+            vim.cmd.colorscheme("onedark")
         end,
     },
 }
