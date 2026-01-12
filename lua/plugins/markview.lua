cddfa77d3641c3d214870ead00d7de06f2856f4b
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2026-01-12 09:32:18 +0100

    feat(plugins): add markview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        dependencies = {
+            "saghen/blink.cmp",
+            "nvim-mini/mini.icons",
+        },
+    },
+}
