35d5e6f0c88a8f1effbd152583e98e364445014e
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2026-01-12 09:31:53 +0100

    refactor(keymaps): add keymap for properly killing a buffer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,7 @@ return {
     event = "VeryLazy",
     keys = {
         { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "[B]uffer [p]in" },
-        { "<leader>bd", "<Cmd>bd<CR>", desc = "[B]uffer [d]elete" },
+        { "<leader>bd", "<Cmd>bp|bd #<CR>", desc = "[B]uffer [d]elete" },
         { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
         { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
         { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
