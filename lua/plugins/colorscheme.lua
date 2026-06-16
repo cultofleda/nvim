3eb799c3c7ae280373c37559b78caf5eb3b0ffa6
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2026-06-16 10:06:17 +0200

    refactor(theme): use nordic theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("catppuccin-macchiato")
+        require("nordic").load()
     end,
 }
